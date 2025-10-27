41c199ff0a1b88b6bc4bddd08e8d32927d6bc23f
Teddy Randby <tedrandby@gmail.com>
2025-10-27 12:34:11 -0400

    Docs

--- qio.h
+++ qio.h
@@ -133,7 +133,7 @@
  *  - The qd freelist is wrapped by a mutex to make it thread-safe.
  *    This could probably be improved by using an atomic qd as the *head of the list*,
  *    meaning we could atomically pop items out without having to lock. Maybe there is an entirely lock-free
- *    implementation we could use as well.
+ *    implementation of the freelist we could use as well.
  *  - The windows implementation with IO completion ports.
  *  - Double check that qio_addrfrom is implemented correctly. I really don't know.
  *  - Further testing and benchmarking
